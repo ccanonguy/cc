f3f26f8bd7616dce299f62cf37e3548c23e54ecd
ccanonguy <singhgurbir10@gmail.com>
2016-05-14 21:06:00 +0530

    uva

--- uva/272.cpp
+++ uva/272.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main () {
-	
+	string s;
+	int count = 0;
+	while (getline(cin, s)) {
+		for (int i=0; i<s.length(); i++) {
+			if (s[i] == '"') {
+				if (count) {
+					cout << "''";
+					count--;
+				}
+				else {
+					cout << "``";
+					count++;
+				}
+			}
+			else {
+				cout << s[i];
+			}
+		}
+		cout << endl;
+	}
 }
