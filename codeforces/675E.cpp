46e552f478018abb03efb8fbbb341961f1b4699d
ccanonguy <singhgurbir10@gmail.com>
2016-05-20 22:46:26 +0530

    codeforces todo 675E

--- codeforces/675E.cpp
+++ codeforces/675E.cpp
@@ -17,8 +17,42 @@ typedef vector<ii> vii;
 int main() {
 	int n;
 	cin >> n;
-	map<int, int> arr(n);
-	fore(i, 1, n) cin >> arr[i];
-
+	int arr[n];
 	int sum = 0;
+	int rho[n][n+1];
+	forn(i,n) {
+		forn(j,n+1) {
+			rho[i][j] = 0;
+		}
+	}
+	//bool visited[n][n+1] = {false};
+	fore(i, 1, n) {
+		cin >> arr[i];
+		for (int j=i+1; j<=arr[i]; j++) {
+			rho[i][j] = 1;
+	//		visited[i][j] = true;
+			sum++;
+		}
+	}
+	//cout << sum;
+
+	for (int j=n; j>=3; j--) {
+		for (int i=j-2; i>=1; i--) {
+			if (!rho[i][j]) {
+				int mini = rho[arr[i]][j];
+				for (int k=i+1; k<arr[i]; k++) {
+					mini = min(mini, rho[k][j]);
+				}
+				rho[i][j] = 1 + mini;
+				//cout << i << " " << j << " " << rho[i][j] << endl;
+				sum += rho[i][j];
+				//visited[i][j] = true;
+			}
+		}
+	}
+
+	cout << sum;
 }
+
+
+//memory limit exceeded
