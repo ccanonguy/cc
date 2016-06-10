e18c20f1c98228b9064b6fd828482a1cc8528a2f
ccanonguy <singhgurbir10@gmail.com>
2016-06-10 20:59:51 +0530

    topcoder srm

--- test.cpp
+++ test.cpp
@@ -1,58 +1,25 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 
 int main() {
-    int a,b;
-    cin >> a >> b;
-    int i;
-    for(i=a;i<=b;i++)
-    {
-        if(i<=9)
-        {
-            if(i==1)
-            {
-            	cout << "one" << endl;
-            }
-            if(i==2)
-            {
-                cout << "two" << endl;
-            }
-            if(i==3)
-            {
-                cout << "three" << endl;
-            }
-            if(i==4)
-            {
-                cout << "four" << endl;
-            }
-            if(i==5)
-            {
-                cout << "five" << endl;
-            }
-            if(i==6)
-            {
-                cout << "six" << endl;
-            }
-            if(i==7)
-            {
-                cout << "seven" << endl;
-            }
-            if(i==8)
-            {
-                cout << "eight" << endl;
-            }
-            if(i==9)
-            {
-                cout << "nine" << endl;
-            }
-            
-        }
-        else if(i%2==0)
-            cout << "even" << endl;
-                
-        else
-            cout << "odd" << endl;
-    }
-    return 0;
- }
+	int n;
+	cin >> n;
+	int totalChars = 2*n -1;
+	for (int i=1; i<=n; i++) {
+		int spaces = totalChars - 2*i +1;
+		spaces /= 2;
+		for (int j=1; j<=spaces; j++) {
+			cout << " ";
+		}
+		for (int j=1; j<=(2*i-1); j++) {
+			if (j%2 == 1)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		for (int j=1; j<=spaces; j++) {
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
